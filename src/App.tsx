77625f974143025200b36ac91571127324828219
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-08-07 23:21:33 +0000

    implemet searching for repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Header } from './Header';
+import { RepoPage } from './repoPage/RepoPage';
 
 
 const queryClient = new QueryClient();
@@ -9,6 +10,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
+      <RepoPage />
     </QueryClientProvider>
   )
 }
