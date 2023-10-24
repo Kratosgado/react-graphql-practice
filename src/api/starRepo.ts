a704d3c92b0cbe359ee22c0a08cd010987660827
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-10-24 14:04:51 +0000

    minor

--- src/api/starRepo.ts
+++ src/api/starRepo.ts
@@ -13,21 +13,21 @@ export const STAR_REPO = gql`
    }
 `
 
-export async function starRepo(repoId:string) {
-   const response = await fetch(
-      process.env.REACT_APP_GITHUB_URL!, {
-      method: 'POST',
-      body: JSON.stringify({
-         query: STAR_REPO,
-         variables: {
-            repoId,
-         },
-      }),
-      headers: {
-         'Content-Type': 'application/json',
-         Authorization: `bearer ${process.env.REACT_APP_GITHUN_PAT}`,
-      }
-   }
-   );
-   await response.json();
-}
+// export async function starRepo(repoId:string) {
+//    const response = await fetch(
+//       process.env.REACT_APP_GITHUB_URL!, {
+//       method: 'POST',
+//       body: JSON.stringify({
+//          query: STAR_REPO,
+//          variables: {
+//             repoId,
+//          },
+//       }),
+//       headers: {
+//          'Content-Type': 'application/json',
+//          Authorization: `bearer ${process.env.REACT_APP_GITHUN_PAT}`,
+//       }
+//    }
+//    );
+//    await response.json();
+// }
